952f6687b7bb3e60b43ae928fcc532348d65565f
Rickard Olsson <rick@olssonr.com>
2015-12-05 16:50:49 +0100

    Add default profile picture

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -24,6 +24,10 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url(*args)
+    "/images/fallback/default_profile_picture.jpg"
+  end
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
