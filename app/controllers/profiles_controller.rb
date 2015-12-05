a0fe28d33507386b30b6e1c8016e8058c50e7a4e
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-12-05 14:39:51 +0100

    Filter profiles on location etc

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,9 @@ class ProfilesController < ApplicationController
   before_action :authenticate_profile!
 
   def index
-    @profiles = Profile.all
+    @profiles = Profile.where(location: current_profile.location)
+                       .where.not(id: current_profile.id)
+                       .where.not(native: current_profile.native)
   end
 
   def show
--- db/seeds/profile.seeds.rb
+++ db/seeds/profile.seeds.rb
@@ -3,7 +3,17 @@ goteborg = Location.create! name: "Göteborg"
 malmo = Location.create! name: "Malmö"
 
 Profile.create!(
-  name: "svensson",
+  name: "native stockholm",
+  email: "sto1@example.com",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  password: "password",
+  location: stockholm,
+  tag_list: "",
+  native: true
+)
+
+Profile.create!(
+  name: "native goteborg 1",
   email: "got1@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
@@ -13,11 +23,41 @@ Profile.create!(
 )
 
 Profile.create!(
-  name: "football lovers",
+  name: "native goteborg 2",
   email: "got2@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
+  tag_list: "food",
+  native: true
+)
+
+Profile.create!(
+  name: "non-native goteborg 1",
+  email: "got3@example.com",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  password: "password",
+  location: goteborg,
   tag_list: "football, disco, fika",
+  native: false
+)
+
+Profile.create!(
+  name: "non-native goteborg 2",
+  email: "got4@example.com",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  password: "password",
+  location: goteborg,
+  tag_list: "football",
+  native: false
+)
+
+Profile.create!(
+  name: "non-native goteborg 3",
+  email: "got5@example.com",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  password: "password",
+  location: goteborg,
+  tag_list: "fika, food",
   native: false
 )
