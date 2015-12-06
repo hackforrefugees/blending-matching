56552165d1d2256ebadfb2ac6d68557f54ffda0b
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-12-06 13:34:47 +0100

    Adds more seeds

--- db/seeds/profile.seeds.rb
+++ db/seeds/profile.seeds.rb
@@ -3,67 +3,67 @@ goteborg = Location.create! name: "Göteborg"
 malmo = Location.create! name: "Malmö"
 
 Profile.create!(
-  name: "native stockholm",
-  email: "sto1@example.com",
+  name: "The royal family",
+  email: "royal@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: stockholm,
-  tag_list: "",
+  tag_list: "charity, duck hunting, politics, religion, partying",
   native: true,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/royal-sweden.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/royal-sweden.jpg"))
 )
 
 Profile.create!(
   name: "The upallnight family",
-  email: "got1@example.com",
+  email: "upallnight@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
-  tag_list: "food, restaurants, fika",
+  tag_list: "partying, disco, food, restaurants, fika",
   native: true,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/the-jackson-family.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/the-jackson-family.jpg"))
 )
 
 Profile.create!(
-  name: "The adventurous family",
-  email: "got2@example.com",
+  name: "The adventure family",
+  email: "adventure@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
-  tag_list: "food",
+  tag_list: "outdoors, hiking, sports, fika",
   native: true,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/minions.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/minions.jpg"))
 )
 
 Profile.create!(
   name: "The culinaries",
-  email: "got3@example.com",
+  email: "culinaries@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
-  tag_list: "football, disco, fika",
+  tag_list: "food, fika",
   native: false,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/the-simpsons.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/the-simpsons.jpg"))
 )
 
 Profile.create!(
   name: "Outdoors family",
-  email: "got4@example.com",
+  email: "outdoors@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
-  tag_list: "football",
+  tag_list: "outdoors, hiking",
   native: false,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/addams-family.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/addams-family.jpg"))
 )
 
 Profile.create!(
   name: "The cozy family",
-  email: "got5@example.com",
+  email: "cozy@example.com",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   password: "password",
   location: goteborg,
-  tag_list: "fika, food",
+  tag_list: "fika, food, outdoors, sunsets",
   native: false,
-  picture: File.open(File.join(Rails.root, '/public/images/seeds/sune.jpg'))
+  picture: File.open(File.join(Rails.root, "/public/images/seeds/sune.jpg"))
 )
