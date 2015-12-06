f0622ec5294b75a436ee96ec08a731f4da012efa
Rickard Olsson <rick@olssonr.com>
2015-12-06 14:06:45 +0100

    Refactor friends

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,12 +3,12 @@ class Profile < ActiveRecord::Base
 
   belongs_to :location, required: true
 
-  has_many :friendships, :foreign_key => "requester_id"
+  has_many :active_friendships, :class_name  => "Friendship", :foreign_key => "requester_id"
   has_many :passive_friendships, :class_name => "Friendship", :foreign_key => "requestee_id"
 
-  has_many :active_friends, -> { where(friendships: { accepted: true}) }, :through => :friendships, :source => :requestee
+  has_many :active_friends, -> { where(friendships: { accepted: true}) }, :through => :active_friendships, :source => :requestee
   has_many :passive_friends, -> { where(friendships: { accepted: true}) }, :through => :passive_friendships, :source => :requester
-  has_many :pending_friends, -> { where(friendships: { accepted: false}) }, :through => :friendships, :source => :requestee
+  has_many :pending_friends, -> { where(friendships: { accepted: false}) }, :through => :active_friendships, :source => :requestee
   has_many :requested_friends, -> { where(friendships: { accepted: false}) }, :through => :passive_friendships, :source => :requester
 
   validates :name, presence: true
@@ -20,35 +20,15 @@ class Profile < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def friendships
+    active_friendships | passive_friendships
+  end
+
   def friends
     active_friends | passive_friends
   end
 
-  def friend_status (profile)
-    friendships = Friendship.where("requester_id = ? AND requestee_id = ?", profile.id, self.id)
-    if (!friendships.empty?)
-      friendship = friendships.first
-      if (friendship.accepted)
-        friendship.status = :accepted
-        return friendship
-      else
-        friendship.status = :awaiting_your_reply
-        return friendship
-      end
-    else
-      friendships = Friendship.where("requester_id = ? AND requestee_id = ?", self.id, profile.id)
-      if (!friendships.empty?)
-        friendship = friendships.first
-        if (friendship.accepted)
-          friendship.status = :accepted
-          return friendship
-        else
-          friendship.status = :pending
-          return friendship
-        end
-      else
-        return nil
-      end
-    end
+  def friendship_for(profile)
+    friendships.find {|friendship| friendship.requester == profile || friendship.requestee == profile}
   end
 end
